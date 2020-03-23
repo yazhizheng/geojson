0a5d668e9135dca78a49d7962a2fa52a55905ade
yazhizheng <62525076+yazhizheng@users.noreply.github.com>
2020-03-22 17:13:38 -0700

    Create wilfredo_lam.js

---
+++ wilfredo_lam.js
@@ -0,0 +1,32 @@
+//the result of converting google my maps kml (one spot) into geojson, using https://mapbox.github.io/togeojson/
+
+
+{
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -82.3520617,
+                    23.1411339,
+                    0
+                ]
+            },
+            "properties": {
+                "name": "Centro Wilfredo Lam",
+                "styleUrl": "#icon-1899-0288D1",
+                "styleHash": "-21fe3a8d",
+                "styleMapHash": {
+                    "normal": "#icon-1899-0288D1-normal",
+                    "highlight": "#icon-1899-0288D1-highlight"
+                },
+                "icon": "https://www.gstatic.com/mapspro/images/stock/503-wht-blank_maps.png",
+                "description": "#museum #surrealism"
+            }
+        }
+    ]
+}
+
+
