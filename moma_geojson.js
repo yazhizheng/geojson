c0eac179123c6fe216732a3da9e3b1005979698d
yazhizheng <62525076+yazhizheng@users.noreply.github.com>
2020-03-24 09:59:49 -0700

    Create moma_geojson.js

---
+++ moma_geojson.js
@@ -0,0 +1,27 @@
+{
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -73.9776216,
+                    40.7614327,
+                    0
+                ]
+            },
+            "properties": {
+                "name": "The Museum of Modern Art",
+                "styleUrl": "#icon-1899-C2185B",
+                "styleHash": "-5f9cb8ad",
+                "styleMapHash": {
+                    "normal": "#icon-1899-C2185B-normal",
+                    "highlight": "#icon-1899-C2185B-highlight"
+                },
+                "icon": "https://www.gstatic.com/mapspro/images/stock/503-wht-blank_maps.png",
+                "description": "#modern #bigandfamous #museum"
+            }
+        }
+    ]
+}
